6545c60d1469368da77c0f7b0e7720c944431682
Laura Padilla <lauvpadilla@gmail.com>
2020-07-02 22:53:38 -0400

    added new fonts and stylez

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
-let font
-let points
+let font;
+let points;
 
 function preload() {
-  font = loadFont('bluunext-bold.otf')
+  font = loadFont("bluunext-bold.otf");
 }
 
 function setup() {
-  createCanvas(1200, 600)
+  createCanvas(1200, 600);
 
-  points = font.textToPoints('punk idols', 100, 330, 180, {
+  points = font.textToPoints("punk idols", 100, 330, 180, {
     sampleFactor: 0.1,
-    simplifyThreshold: 0
-  })
+    simplifyThreshold: 0,
+  });
 }
 
 // draws circles first
 // then draw the lines (vertex)
 function draw() {
   // noise level
-  const nl = 0.01
-  background('black')
+  const nl = 0.01;
+  background("black");
 
   //   fill for anything below this
-  fill('#e6ff2f')
-  noStroke()
+  fill("#e6ff2f");
+  noStroke();
 
-  points.forEach(point => {
+  points.forEach((point) => {
     // how far away the point is from mouse
-    const distance = createVector(point.x - mouseX, point.y - mouseY)
+    const distance = createVector(point.x - mouseX, point.y - mouseY);
     // take distance and multiply it by 60 and magnify it
-    const distortion = distance.mult(60 / distance.mag())
+    const distortion = distance.mult(60 / distance.mag());
 
-    circle(point.x + distortion.x, point.y + distortion.y, 5)
-  })
+    circle(point.x + distortion.x, point.y + distortion.y, 5);
+  });
 
   // this applies to the vertext below and
   // won't affect what's above
-  noFill()
-  stroke('#e6ff2f')
+  noFill();
+  stroke("#e6ff2f");
 
-  beginShape()
-  points.forEach(point => {
-    const distance = createVector(point.x - mouseX, point.y - mouseY)
-    const distortion = distance.mult(60 / distance.mag())
-    
-    const nx = 40 * noise(nl * point.x, nl * point.y, nl * frameCount) - 20
-    const ny = 40 * noise(nl * point.x, nl * point.y, nl * frameCount) - 20
+  beginShape();
+  points.forEach((point) => {
+    const distance = createVector(point.x - mouseX, point.y - mouseY);
+    const distortion = distance.mult(60 / distance.mag());
 
-    vertex(point.x + distortion.x + nx, point.y + distortion.y + ny)
-  })
-  endShape()
+    const nx = 40 * noise(nl * point.x, nl * point.y, nl * frameCount) - 20;
+    const ny = 40 * noise(nl * point.x, nl * point.y, nl * frameCount) - 20;
+
+    vertex(point.x + distortion.x + nx, point.y + distortion.y + ny);
+  });
+  endShape();
 }
