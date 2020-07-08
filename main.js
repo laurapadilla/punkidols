1cae5ac11aaca7b0a40cfdd3468aaaf22d017652
Laura Padilla <lauvpadilla@gmail.com>
2020-07-08 09:14:07 -0400

    add patti's modal

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ artists.forEach((artist) => {
     artist.style.transform = "";
   });
 });
+
+function makeMarquee() {
+  const title = "Patti Smith 🍒 ";
+
+  // an array is a list of things
+  const marqueeText = new Array(100).fill(title).join("  ");
+
+  // 1. we want to grab out marquee span form the html
+  // 2. we want to set our repeating title as the content
+
+  const marquee = document.querySelector(".marquee span");
+  marquee.innerHTML = marqueeText;
+}
+
+makeMarquee();
